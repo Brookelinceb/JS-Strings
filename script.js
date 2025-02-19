6645b9ae4ba9a2452a8170f6dec4fe2502f48b8a
Brookelinceb <brookelinceb@yahoo.com>
2025-02-18 20:55:16 -0600

    added examples from class

--- script.js
+++ script.js
@@ -2,39 +2,36 @@
 
 // 1. Alerts, Prompts, and document.write()
 // Activity: Create an alert box that welcomes the user
-    let alertMessage = "This is my first text in a varibale";
-    alert(alertMessage);
+//let alertMessage = "This is my first string in variable" 
+//alert("alertMessage"); // 
+
 // Activity: Use a prompt to ask for the user's name and display it on the page
-    let username = prompt("What is your username?");
-    console.log("the username is "+username);
+ // let userName = prompt("What is your user name?");
+ // console.log(userName);
 // Activity: Use document.write() to display a message on the page
-    let favHobby = prompt("What is your favorite hobby?");
-    document.write("My favorite hobby is " + favHobby);
-    
+  // let favHobby = prompt("What is your favorite hobby?");
+//    document.write("My favorite hobby is "+ favHobby);
 // 2. String Concatenation
 // Activity: Concatenate two strings and log the result to the console
-    let str1 = "Today is Monday";
-    let str2 = " the date is February 17th, 2025";
+//   let str1 = "Today is monday";
+//   let str2 = "the date is Feburary 18th, 2025";
 
-    console.log(str2 + ". I love Mondays!" + str1);
+//   console.log(str1 + str2 + ".I love Mondays");
 // 3. Template Literals
-// Activity: Use template literals to create a sentence with variables
-    let firstName = 'Lexius';
-    let lastName = 'Waltar';
-    let goodNightMessage = `Good evening, ${firstName} ${lastName}. Have a nice night!`
-    console.log(message);
-
+// Activity: Use template literals to creat`
+// let firstName = "Brooke";
+// let lastName = "Seal";
+// let message = `Good evening, ${firstName} ${lastName}. HAve a nice night!`
+// console.log(message);
 // 4. String Methods
 // Activity: Experiment with different string methods like .length, .toUpperCase(), .trim(), etc.
+let message = "  Javascript is awesome!   ";
 
-    let message = "  Javascript is awesome!   ";
-
-    console.log(message.length); // 27
-    console.log(message.toUpperCase()); //  JAVASCRIPT IS AWESOME!  
-    console.log(message.trim()); //Javascript is awesome!
-    console.log(message.includes("awesome")); //true
-    console.log(message.slice(0,-5)); //  Jav
-
+// console.log (message.length); //27
+// console.log (message.toUpperCase()); //  JAVASCRIPT IS AWESOME!
+// console.log (message.trim()); //Javascript is awesome
+// console.log (message.includes("awesome")); //True
+// console.log (message.slice(0,6)); //  Java
 
 // 5. Mini Project: Interactive Story
 // Activity: Ask the user for inputs and create a short story using their responses
